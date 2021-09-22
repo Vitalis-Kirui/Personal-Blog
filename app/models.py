b7e65a7f715f63b6b21cd0817c7cdd31e00a2d16
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-22 16:55:32 +0300

    creating qoute class model and a request function to random quotes site

---
+++ app/models.py
@@ -0,0 +1,8 @@
+#Quotes class
+class Quote:
+    """
+    Class for creating our random quotes.
+    """
+    def __init__(self, author, quote):
+        self.author = author
+        self.quote = quote
