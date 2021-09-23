28fb30a333cfb150612f45a5e72e53159a7728df
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-23 08:16:31 +0300

    adding user profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..requests import get_quote
 from flask_login import login_required
+from ..models import User
 
 @main.route('/', methods=["GET"])
 def index():
@@ -11,3 +12,12 @@ def index():
     '''
     quote = get_quote()
     return render_template('index.html', quote=quote)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
