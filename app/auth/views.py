1f5b6577b15a7f309358e2e4bd62409e0345e70b
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-23 20:46:03 +0300

    pushing email welcome text

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
 from . import auth
+from ..email import mail_message
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -12,6 +13,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to F-Society","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
