213893ba692027418a747d70df1443d81d7f0d0f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-23 08:47:50 +0300

    pushing profile picture updload view function,form and html file

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Computer757575.@localhost/fsociety'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     '''
