5c2203747b836e2b7e3def4d2033c2ab5e823536
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-24 11:13:59 +0300

    pushing navbar styles

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from .forms import UpdateProfile
 from .. import db,photos
-from ..requests import get_quote
+# from ..requests import get_quote
 from flask_login import current_user, login_required
 from ..models import User, Blog, Comment,Subscriber
 from app.main.forms import BlogForm,CommentForm
@@ -11,9 +11,9 @@ from datetime import datetime
 @main.route("/", methods=["GET", "BLOG"])
 def index():
     blogs = Blog.get_all_blogs()
-    quote = get_quote()
+    # quote = get_quote()
 
-    return render_template("index.html", blogs=blogs,quote=quote)
+    return render_template("index.html", blogs=blogs)
 
 @main.route("/blog/new", methods=["POST", "GET"])
 @login_required
