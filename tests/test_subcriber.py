88d077591a0529a45f8c2e34ad0b5e10c77ddcde
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-24 19:19:39 +0300

    pushing test class for subcriber  class

---
+++ tests/test_subcriber.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Subscriber
+
+class TestSubscriber(unittest.TestCase):
+    def setUp(self):
+        self.mail = Subscriber(email = "sambu@gmail.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mail, Subscriber))
+
+    def test_init(self):
+        self.assertEqual(self.mail.email,"sambu@gmail.com")
+
+    def test_save_comment(self):
+        self.mail.save_subscriber()
+        self.assertTrue(len(Subscriber.query.all()) > 0)
