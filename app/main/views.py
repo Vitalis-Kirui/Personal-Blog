9e7e3090c22bb0844479dd4cdd385cf558cb9009
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-25 09:14:00 +0300

    removing simplemde

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,6 @@ from flask_login import current_user, login_required
 from ..models import User, Blog, Comment,Subscriber
 from app.main.forms import BlogForm,CommentForm
 from datetime import datetime
-# import markdown2 
 
 @main.route("/", methods=["GET", "BLOG"])
 def index():
